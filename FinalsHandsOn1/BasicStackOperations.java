2dccae950dacbcb16870a3507934348003b640ef
Ian Paulo <ianpaulomelu@gmail.com>
2023-04-22 11:22:15 +0000

    Prob1

--- FinalsHandsOn1/BasicStackOperations.java
+++ FinalsHandsOn1/BasicStackOperations.java
@@ -9,8 +9,10 @@ class BasicStackOperations{
         for(int i=0;i<stack_size;i++)
             stack.push(rn.nextInt(100));
         System.out.println("Original stack elements: " + stack);
-        for(int i=0;i<rn.nextInt(stack_size - 1) + 1;i++)
+        int popCount = rn.nextInt(stack_size - 1) + 1;
+        for(int i=0;i<popCount;i++)
             stack.pop();
+        System.out.println(popCount + " pop functions are executed");
         System.out.println("Stack after pop operations: " + stack);
         System.out.println("The top element is: " + stack.peek());
         System.out.println("Is stack empty? " + stack.isEmpty());
